d9c2c10c872e1582f31a460e21bf3d9891408f72
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-15 19:57:10 +0000

    done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf ("\"Programming is like building a multilingual puzzle\n");
+    return (0);
+}
