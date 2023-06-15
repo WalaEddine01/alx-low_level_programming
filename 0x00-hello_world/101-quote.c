5ed135e917adcfd919b8a9be3791700fb76e5c57
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-15 16:23:56 -0700

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point 
+ *
+ * Description: print a qoute using write fun 
+ * wirite (int fd , const,size_t)
+ *
+ * Return: 1 (not success)
+ */
+int main(void)
+{
+	char qu[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1,qu,59);
+	return (1)
+}
