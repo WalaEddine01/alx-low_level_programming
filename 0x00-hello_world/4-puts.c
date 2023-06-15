6c900eda4281ab276571574949665c5ce55f84c1
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-15 15:21:43 +0000

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*walaaeddine*/
+/**  
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)  
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
