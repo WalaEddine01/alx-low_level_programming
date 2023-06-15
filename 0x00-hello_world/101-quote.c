b7d204d0bcc542707e5535343d7a17853f5d0e19
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-15 16:33:23 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,8 @@
  */
 int main(void)
 {
-	char qu[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" ;
-	write(1 , qu , 59) ;
+	char qu[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" ;
+
+	write(1,qu,59) ;
 	return (1);
 }
