b6a13d155406497688a0a4d775adc1f6d090643b
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-18 22:01:48 +0000

    done

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
- * main - entry point 
- * Descriotion: print all charecters of alphabets 
+ * main - entry point
+ * Descriotion: print all charecters of alphabets
  * Return: always 0 (success)
  */
-int main (void)
+int main(void)
 {
 	char le;
 
 	for (le = 'a' ; le <= 'z' ; le++)
 		putchar(le);
 	putchar('\n');
-	return(0);
+	return (0);
 }
 
