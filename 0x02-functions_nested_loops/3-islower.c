835d61234d4e09c8eb2c2ef518e2d39e1b284e12
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-20 15:27:55 +0000

    auto push

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _islower - return 1 if is lower case
- * @param c the first parameter
- * @return 1 if is lowercase
- * @return 0 otherwise
+ * _islower - return 1 if 'c' is lowercase
+ * description: return 1 if is lowercase
+ * @c the first parameter
+ * return: 1 if 'c' is lowercase return: 0 (success) otherwise
  */
 int _islower(int c)
 {
