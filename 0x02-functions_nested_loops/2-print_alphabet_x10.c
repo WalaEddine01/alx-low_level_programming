3cd5a8ccbffe43ea179a75222451cfd6e4db13b4
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-20 14:19:57 +0000

    auto push

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print8alphabet_x10 - print alphabets 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int j;
+	char i;
+
+	for (j = 0 ; j <= 9 ; j++)
+	{
+		for (i = 'a' ; i <= 'z' ; i++)
+		{
+			_putchar(i);
+		}
+	}
+}
