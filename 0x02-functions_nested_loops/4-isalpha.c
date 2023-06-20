35919af39ae72d20e587d4187658a839d79f6754
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-20 15:48:56 +0000

    auto push

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,7 +8,15 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
