0015a9e6f37e2f1ca0cf0d4e1cca46734a077e9a
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-21 21:00:40 +0000

    auto push

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,20 +16,22 @@ void print_times_table(int n)
 			for (j = 1 ; j <= n ; j++)
 			{	_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
 				a = i * j;
-				if (a > 9)
+				if (a <= 9)
+					_putchar(' ');
+				if(a <= 99)
+					_putchar(' ');
+				if (a > 99)
 				{
-					_putchar((a / 10) + '0');
-					_putchar((a % 10) + '0');
+					_putchar((a / 100) + '0');
+					_putchar((a / 10) % 10 + '0');
 				}
 				else
 				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(a + '0');
+					if (a <= 99 && a >= 10)
+						_putchar((a / 10) + '0');
 				}
+					_putchar((a % 10) + '0');
 			}
 			_putchar('\n');
 		}
