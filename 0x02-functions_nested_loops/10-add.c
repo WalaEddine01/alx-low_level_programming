cf638faf948c737f02ecb94c603be479970b4920
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-21 17:43:09 +0000

    auto push

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * add - adds two integers and returns the result
- * @a and @b two intergers two find the som
+ * @a: first integer
+ * @b: second integer
  * Return: som of @a and @b
  */
 int add(int a, int b)
