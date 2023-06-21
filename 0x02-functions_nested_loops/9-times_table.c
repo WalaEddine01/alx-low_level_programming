a1591fb84c62f44b20ff13aaee27f4ca715149c9
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-21 16:34:21 +0000

    auto push

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,28 +10,20 @@ void times_table(void)
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
-		for (j = 0 ; j <= 9 ; j++)
+		_putchar(48);
+		for (j = 1 ; j <= 9 ; j++)
 		{
-			a = (i * j);
-			if (a <= 9)
-			{
-				_putchar(a + '0');
-				if (j != 9)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(',');
-				}
-			}
-			else
+			_putchar(',');
+			_putchar(' ');
+			a = i * j;
+			if (a > 9)
 			{
 				_putchar((a / 10) + '0');
 				_putchar((a % 10) + '0');
-				if (j != 9)
-				{
-					_putchar(' ');
-					_putchar(',');
-				}
+			}
+			else
+			{
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
