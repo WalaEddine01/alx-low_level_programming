297a54247a6c53776b1c9cfaef9b19655d2a8b3f
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-22 15:36:41 +0000

    auto commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (c > a && c > b)
+	else if (c >= a && c >= b)
 	{
 		largest = c;
 	}
