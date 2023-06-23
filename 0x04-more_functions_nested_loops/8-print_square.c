8c6e9c1f23b7e089f2ae0f96593b55808801b5ad
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-23 17:31:34 +0000

    i made the changes

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square - draws a square in the terminal
+ * @n : is the size of the square
+ * don't return a value
+ */
+void print_square(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
