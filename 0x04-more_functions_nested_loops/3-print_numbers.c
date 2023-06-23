5d79bd4f7b2c7c41fd25391788a5c00f512f969c
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-23 15:11:16 +0000

    i made the changes

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * no parameter
+ */
+void print_numbers(void)
+{
+	int i;
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
