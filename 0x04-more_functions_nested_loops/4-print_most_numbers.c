f9499a36647ed1cd69c65dfbabe45c2a6aada43c
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-23 15:21:43 +0000

    i made the changes

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,12 +6,12 @@
 void print_most_numbers(void)
 {
 	int i;
+
 	for (i = 0; i <= 9; i++)
 	{
-		if (i != 2 || i != 4)
-		{
-			_putchar(i + 48);
-		}
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i + 48);
 	}
 	_putchar('\n');
 }
