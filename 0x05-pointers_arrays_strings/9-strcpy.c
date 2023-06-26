951b16bfb0befb923827bce7209c25d41a2a9bba
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-26 21:53:52 +0000

    Changes made at 2023-06-26 21:53:52

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,16 +7,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i, n;
+	int i;
 
+	i = 0;
 	while (src[i] != 0)
 	{
 		i++;
-	}
-	n = i;
-	for (i = 0; i < n; i++)
-	{
 		dest[i] = src[i];
 	}
+	dest[i] = 0;
 	return (dest);
 }
