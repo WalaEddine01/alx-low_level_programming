0272a347ab687138421f2f580b234a67db652ca9
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-26 14:17:10 +0000

    Changes made at 2023-06-26 14:17:10

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: string
+ * don't return
+ */
+void _puts(char *str)
+{
+	int i;
+
+	while (str[i] != 0)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
