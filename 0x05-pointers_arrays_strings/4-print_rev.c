af403b89aa09ea51b6937cc8b4da4b254698bcaa
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-26 16:14:10 +0000

    Changes made at 2023-06-26 16:14:10

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - print a string in revers
+ * @s: string input
+ * don't return
+ */
+void print_rev(char *s)
+{
+	int i, a;
+
+	if (str == 0)
+		return;
+	a = 0;
+	i = 0;
+	while (s[i] != 0)
+	{
+		a++;
+		i++;
+	}
+	for (i = a; i > 0; i--)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
