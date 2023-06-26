59e1600daf44d6f0ba83d4db80c4887088b01ebc
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-26 21:46:49 +0000

    Changes made at 2023-06-26 21:46:49

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strcpy - copies the string to another
+ * @dest: output
+ * @src: input
+ * Return: string @dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	while (src[i] != 0)
+	{
+		i++;
+	}
+	n = i;
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (*dest);
+}
