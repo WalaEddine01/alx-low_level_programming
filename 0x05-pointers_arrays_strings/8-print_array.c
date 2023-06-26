b6506fde5e4e61edf6942bb1aed437c28826392e
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-26 21:38:50 +0000

    Changes made at 2023-06-26 21:38:50

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,12 +14,12 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i != n)
+		if (i != n - 1)
 		{
 			printf(",");
 			printf(" ");
 		}
 	}
-	_putchar('\n');
+	printf("\n");
 }
 
