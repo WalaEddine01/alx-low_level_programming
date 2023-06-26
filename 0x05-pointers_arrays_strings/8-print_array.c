d265b3aca5252bd74c5786ff629391a6cc62b6ee
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-26 21:27:26 +0000

    Changes made at 2023-06-26 21:27:26

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,24 @@
 #include "main.h"
 /**
+ * print_array - print n elements of an array
+ * @a: first input
+ * @n: second input number of elements
+ * don't return a value
  *
  */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(a);
+		if (i != n)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
 
