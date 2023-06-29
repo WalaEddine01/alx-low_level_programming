c131a70095cb50631dd401dc1b886a2e3f462303
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-29 09:04:03 +0000

    the last changes in this progect

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,17 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-int _putchar(char c);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
