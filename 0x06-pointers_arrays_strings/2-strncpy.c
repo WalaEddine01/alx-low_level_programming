5016a146bc7f50e75f414ea27d6c245cc3ebbddb
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-30 14:25:22 +0000

    Changes made at 2023-06-30 14:25:22

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,10 @@
 #include "main.h"
 /**
- *
+ * _strncpy - copies strings
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ * Return: @dest string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
