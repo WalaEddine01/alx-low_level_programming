dfc454e7ebb18b5ea2d5a1ef72ad2e4b66a2274c
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-30 17:12:36 +0000

    Changes made at 2023-06-30 17:12:36

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * string_toupper - convert a string to to uppercase
+ * @b: input
+ * Return: string in uppercase
+ *
+ */
+char *string_toupper(char *b)
+{
+	int i, a;
+
+	i = 0;
+	a = 0;
+	while (b[i] != 0)
+	{
+		a++;
+		i++;
+	}
+	for (i = 0; i < a; i++)
+	{
+		if (b[i] >= 'a' || b[i] <= 'z')
+		{
+			b[i] -= 32;
+		}
+		i++;
+	}
+	return (b);
+}
