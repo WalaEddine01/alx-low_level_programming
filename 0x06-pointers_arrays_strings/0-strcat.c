263f305145623040dccdda77b00dd1333c1d0cf5
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-30 09:08:42 +0000

    Changes made at 2023-06-30 09:08:42

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,13 @@ char *_strcat(char *dest, char *src)
 {
 	while (*dest != 0)
 	{
-		*dest++;
+		dest++;
 	}
 	while (*src != 0)
 	{
 		*dest = *src;
-		*src++;
-		*dest++;
+		src++;
+		dest++;
 	}
 	return (dest);
 }
