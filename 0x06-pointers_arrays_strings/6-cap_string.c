1f3ce7ae0c9b7981b15a4ea4dc3c9c4036719f71
WalaEddine01 <walaaeddine33@gmail.com>
2023-06-30 21:02:57 +0000

    Changes made at 2023-06-30 21:02:56

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * *cap_string - capitalizes all words
+ * @b: input
+ * Return: @d compitalized string
+ */
+char *cap_string(char *b)
+{
+	int i;
+
+	i = 0;
+	while (b[i] != 0)
+	{
+		if (b[i] == ' ' || b[i] == '\t' || b[i] == '\n' ||
+				b[i] == ',' || b[i] == ';' ||
+				b[i] == '.' || b[i] == '!' ||
+				b[i] == '?' || b[i] == '"' ||
+				b[i] == '(' || b[i] == ')' ||
+				b[i] == '{' || b[i] == '}')
+		{
+			if (b[i + 1] >= 'a' && b[i + 1] <= 'z')
+			{
+				b[i + 1] = b[i + 1] - 32;
+			}
+		}
+		i++;
+	}
+	return (b);
+}
