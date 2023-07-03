c43e551ec51ce77c45b6f2e42ab4840594eb8783
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-03 15:05:48 +0000

    Changes made at 2023-07-03 15:05:48

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,11 +3,11 @@
  * _strchr - locates a character in a string
  * @s: input string
  * @c: the character
- * Return: pointer to the first occurrence of @c
+ * Return: pointer to the first occurrence of @c;
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	unsigned int i;
 
 	i = 0;
 	while (s[i] != 0)
