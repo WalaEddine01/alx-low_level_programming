eaf64b059ca36d6e319fbac6678df94ac33ff204
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-03 21:22:30 +0000

    Changes made at 2023-07-03 21:22:30

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: input string
+ * @needle:substring
+ * Return: pointer to the begining of the located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k;
+
+	for (i; haystack[i] != 0; i++)
+	{
+		for (j = 0; needle[j] != 0; j++)
+		{
+			k = i;
+			if (needle[j] == haystack[k])
+			{
+				while (needle[j] == haystack[k])
+				{
+					k++;
+					j++;
+				}
+				if (needle[j] == 0)
+					return (haystack + i);
+			}
+		}
+	}
+	return (0);
+}
