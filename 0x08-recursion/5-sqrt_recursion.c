028737ab77ce555076307964d438e2d360c287a0
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-06 12:40:23 +0000

    Changes made at 2023-07-06 12:40:23

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,5 +6,19 @@
  */
 int _sqrt_recursion(int n)
 {
-	return (sqrt (n));
+	return (squar_recu(n, 1));
+}
+/**
+ * squar_recu - find square root
+ * @n: input
+ * @v: square root
+ * Return: square root value
+ */
+int squar_recu(int n, int v)
+{
+	if (v * v ==  n)
+		return (v);
+	if (v * v < n)
+		return (squar_recu(n, val + 1));
+	return (-1);
 }
