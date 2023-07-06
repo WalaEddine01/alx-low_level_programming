411a29619d6e0d399deeedccbc5ecd46e3b19a59
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-06 10:17:26 +0000

    Changes made at 2023-07-06 10:17:26

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns length of a string
+ * @s: input string
+ * Return: length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+	return (_strlen_recursion(s + 1) + 1);
+}
