a4073d63682f93ad8aa140a941575ed16bdcfe24
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-07 21:01:19 +0000

    Changes made at 2023-07-07 21:01:19

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - prints its name
+ * @argc: count command line arg
+ * @argv: array command line-arg
+ * Return: 0
+ */
+int main(int argc, char argv[])
+{
+	printf("%s", argv[argc]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+this is a argc, argv project
