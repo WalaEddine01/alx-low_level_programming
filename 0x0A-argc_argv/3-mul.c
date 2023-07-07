bffa35ebca1c812774a87125afa308b3a3b990b5
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-07 22:25:05 +0000

    Changes made at 2023-07-07 22:25:05

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc: count command line arg
+ * @argv: array command line-arg
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	atoi(argv[2]) = atoi(argv[2]) * atoi(argv[1]);
+	printf("%d\n", atoi(argv[2]);
+
+	return (0);
+}
