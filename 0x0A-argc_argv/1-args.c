25c0d7178229a274fee0dfafe87edf63d22ca506
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-07 21:28:45 +0000

    Changes made at 2023-07-07 21:28:45

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the number of arguments
+ * @argc: count command line arg
+ * @argv: array command line-arg
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
