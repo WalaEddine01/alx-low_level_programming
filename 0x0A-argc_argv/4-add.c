4267e450981f04b9eb6a1fd5ea8e53e15aad8428
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-08 21:58:09 +0000

    Changes made at 2023-07-08 21:58:09

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,6 +14,11 @@ int main(int argc, char *argv[])
 	j = 0;
 	for (i = 0; i < argc; i++)
 	{
+		if ((atoi(argv[i]) < '0' || atoi(argv[i]) > '9'))
+		{
+			printf("Error\n");
+			return (1);
+		}
 		j = j + atoi(argv[i]);
 	}
 	printf("%d\n", j);
