07449a57a4605a1922414be9532ee55bfc79a9a3
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-10 16:49:05 +0000

    Changes made at 2023-07-10 16:49:05

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,10 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	j = 0;
 	a = 0;
-	if (s1 == 0 || s2 == 0)
-		return (0);
+	if (s1 == 0)
+		s1 = '\n';
+	if (s2 == 0)
+		s2 = '\0';
 	while (*s1 != 0)
 		i++;
 	while (*s2 != 0)
