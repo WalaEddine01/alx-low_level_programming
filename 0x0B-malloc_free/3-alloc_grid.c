ecf3bd52adff7c40dda2ffb268a34c072bb2d369
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-11 14:37:25 +0000

    Changes made at 2023-07-11 14:37:25

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,4 +1,36 @@
 #include "main.h"
 /**
+ * alloc_grid - returb a pointer to matrix
+ * @width: width of the matrix
+ * @height: height of the matrix
+ * Return: pointer to matrix or 0 if it fails
  */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **p;
 
+	if (with == 0 || height == 0)
+		return (0);
+	p = malloc(sizeof(int *) * height);
+	if (p == 0)
+		return (0);
+	for (i = 0; i < height; i++)
+	{
+		p[i] = malloc(sizeof(int) * width);
+		if (p[i] == 0)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(p[j]);
+			}
+			free(p);
+			return (0);
+		}
+		for (j = 0; j < wight; j++)
+		{
+			p[i][j] = 0;
+		}
+	}
+	return (p);
+}
