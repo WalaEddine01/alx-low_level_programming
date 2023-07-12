7036207ccfd1220bcf9b66b3fd502f0e8f270880
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-12 17:49:27 +0000

    Changes made at 2023-07-12 17:49:27

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * free_grid - frees a mattrix grid
+ * @height: height of the matrix
+ * @grid: input matrix
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i, j;
+
+	if (height == 0)
+		return (0);
+	if (grid == 0)
+		return (0);
+	for (i = 0; i < height; i++)
+	{
+		free(p[i]);
+	}
+}
