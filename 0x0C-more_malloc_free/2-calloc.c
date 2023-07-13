c35608e4c48fe20606ca243c334aa06443379069
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-13 21:30:56 +0000

    Changes made at 2023-07-13 21:30:56

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,14 +8,13 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	char *p;
+	void *p;
 
 	if (nmemb == 0 || size == 0)
 		return (0);
 	p = malloc(nmemb * size);
 	if (p == 0)
 		return (0);
-	for (i = 0; i < nmemb; i++)
-		p[i] = 0;
+	memset(p, 0, nmemb * size);
 	return (p);
 }
