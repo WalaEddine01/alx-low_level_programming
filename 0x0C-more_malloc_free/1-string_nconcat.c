31b04cda3daffc2675d75a6d16f91348f75837af
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-13 20:36:07 +0000

    Changes made at 2023-07-13 20:36:07

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,32 @@
 #include "main.h"
 /**
- * string_nconcat - cocatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: number of bytes from s2
- * Return: concatenated string
- */
+ * string_nconcat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number of bytes
+ * Return: pointer to the concatenated string
+*/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int c, i, j, a, b;
 	char *p;
+	unsigned int i, j, a, b;
 
-	if (s1 == 0 || s2 == 0)
-		return (0);
-
-	a = strlen(s1);
-	b = strlen(s2);
-	c = a + b;
-	p = malloc(sizeof(char) * (c + 1));
+	if (s1 == 0)
+		s1 = "";
+	if (s2 == 0)
+		s2 = "";
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	if (n >= j)
+		n = j;
+	p = malloc(sizeof(char) * (i + n + 1));
 	if (p == 0)
 		return (0);
-	for (i = 0; i < a; i++)
-	{
-		p[i] = s1[i];
-	}
-	if (n >= b)
-	{
-		for (j = 0; j < b; j++)
-		{
-			p[i] = s2[j];
-			i++;
-		}
-	}
-	else
-	{
-		for (j = 0; j < n; j++)
-		{
-			p[i] = s2[j];
-			i++;
-		}
-	}
+	for (a = 0; a < i; a++)
+		p[a] = s1[a];
+	for (b = 0; b < n; b++)
+		p[a + b] = s2[b];
 	return (p);
 }
