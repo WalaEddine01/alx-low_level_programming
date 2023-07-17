2bbc42ef503d24b4a84dc6cdd59e8f4fe7895244
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-17 21:25:35 +0000

    Changes made at 2023-07-17 21:25:35

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+is is 0x0E-structures typedef project
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
+}
