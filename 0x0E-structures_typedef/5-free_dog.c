d5ea4d2ddd552f74b5fc2060abe064505cab5753
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-18 17:47:01 +0000

    Changes made at 2023-07-18 17:47:01

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees dogs
+ * @d: pointer to structur
+ * Return: Nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
