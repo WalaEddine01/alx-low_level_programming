9daf000931f29ba169f592828291c76a319afade
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-20 17:31:44 +0000

    Changes made at 2023-07-20 17:31:44

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+/**
+ * get_op_func - returns pointer to right op func
+ * @s: string iput
+ * Return: the right function pointer
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+	while (i < 5)
+	{
+		if (s && s[0] == ops[i].op[0] && s[1] == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,8 @@
+#include "3-calc.h"
+/**
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
