21e95ae2ae9b9786e030b14c914e36afc4bf25c7
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-28 19:16:14 +0000

    Changes made at 2023-07-28 19:16:14

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@ void _constructor(void) __attribute__ ((constructor));
  */
 void _constructor(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house
-			upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
