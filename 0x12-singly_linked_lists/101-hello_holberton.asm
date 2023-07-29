62ffc8de9e64e511c0fa7cb1dfcfbc660c018a36
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-29 08:16:31 +0000

    Changes made at 2023-07-29 08:16:31

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,9 @@
-section	.data
-	msg	db	"hello, holberton", 0xa, 0
 section	.text
 	extern	printf
 	global	_start
 _start:
 	mov	edi,	msg
 	mov	eax,	0
-	call 	printf
+	call	printf
+section	.data
+msg	db	"hello, holberton", 0xa, 0
