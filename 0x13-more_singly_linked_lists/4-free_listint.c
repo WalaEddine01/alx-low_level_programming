44724b0d1f0441f1b619eab59f92691b32e43899
WalaEddine01 <walaaeddine33@gmail.com>
2023-07-31 13:22:08 +0000

    Changes made at 2023-07-31 13:22:08

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - frees the list
+ * @head: input list
+ * Return: Nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+		while (head != NULL)
+		{
+			ptr = head;
+			head = head->next;
+			free(ptr);
+		}
+}
