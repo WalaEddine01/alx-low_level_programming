43d32f586ac2d0854c926b4e7eb0026626258b26
WalaEddine01 <walaaeddine33@gmail.com>
2023-08-01 20:23:56 +0000

    Changes made at 2023-08-01 20:23:56

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * free_listint_safe - frees the list
+ * @h: input list
+ * Return: size of the list free'd
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *ptr, *ptr2;
+	int n = 0;
+
+	if (!h || !*h)
+		return (0);
+	ptr2 = *h;
+	while (ptr2)
+	{
+		ptr = ptr2;
+		ptr2 = ptr2->next;
+		free(ptr);
+		n++;
+	}
+	*h = NULL;
+	return (n);
+}
