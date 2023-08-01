cc1182afcbf0123e5e1593332e10e0b4caa684d2
WalaEddine01 <walaaeddine33@gmail.com>
2023-08-01 12:44:10 +0000

    Changes made at 2023-08-01 12:44:10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	ptr = *head;
 	if (index != 0)
 	{
-		if (head != 0)
+		if (head != 0 || *head != NULL)
 		{
 			while (i < index - 1)
 			{
