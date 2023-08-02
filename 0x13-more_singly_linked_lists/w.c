6add487efa117ea21a08a189143a0f88c5f3b38f
WalaEddine01 <walaaeddine33@gmail.com>
2023-08-02 10:38:42 +0000

    Changes made at 2023-08-02 10:38:42

--- 0x13-more_singly_linked_lists/w.c
+++ 0x13-more_singly_linked_lists/w.c
@@ -1,41 +1,42 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
+listint_t *_add_node(listint_t **head, int n)
+{
+	listint_t *tmp;
+
+	tmp = malloc(sizeof(*tmp));
+	if (!tmp)
+		return (NULL);
+	tmp->n = n;
+	tmp->next = *head;
+	*head = tmp;
+	return (tmp);
+}
 int main(void)
 {
-    listint_t *head;
-    listint_t *head2;
-    listint_t *node;
+	listint_t *head;
+	listint_t *node1;
+    listint_t *node2;
+    listint_t *node3;
+    listint_t *node4;
+    listint_t *node5;
+	size_t n;
 
-    head2 = NULL;
-    add_nodeint(&head2, 0);
-    add_nodeint(&head2, 1);
-    add_nodeint(&head2, 2);
-    add_nodeint(&head2, 3);
-    add_nodeint(&head2, 4);
-    add_nodeint(&head2, 98);
-    add_nodeint(&head2, 402);
-    add_nodeint(&head2, 1024);
-    print_listint_safe(head2);
-    head = NULL;
-    node = add_nodeint(&head, 0);
-    add_nodeint(&head, 1);
-    add_nodeint(&head, 2);
-    add_nodeint(&head, 3);
-    add_nodeint(&head, 4);
-    node->next = add_nodeint(&head, 98);
-    add_nodeint(&head, 402);
-    add_nodeint(&head, 1024);
-    print_listint_safe(head);
-    free_listint_safe(&head2);
-    free_listint_safe(&head);
-    printf("%p, %p\n", (void *)head2, (void *)head);
-    return (0);
+	head = NULL;
+    /* 1 -> 4 -> 3 -> 2 -> 5 -> (3) */
+	node5 = _add_node(&head, 5);
+    node4 = _add_node(&head, 4);
+    node3 = _add_node(&head, 3);
+    node2 = _add_node(&head, 2);
+    node1 = _add_node(&head, 1);
+
+    node1->next = node4;
+    node4->next = node3;
+    node3->next = node2;
+    node2->next = node5;
+    node5->next = node3;
+    
+	n = print_listint_safe(head);
+	printf("%lu\n", n);
+	return (0);
 }
