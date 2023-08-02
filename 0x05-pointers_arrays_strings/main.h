fe73998a8f6cd8227dbca6b70dc529a0e3bcda88
WalaEddine01 <walaaeddine33@gmail.com>
2023-08-02 20:00:39 +0000

    change files

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,15 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
- char *_strcpy(char *dest, char *src);
- int _atoi(char *s);
-#endif /* MAIN_H */
+#include <unistd.h>
+#include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
--- 0x0C-more_malloc_free/auto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-commit_message="Changes made at $(date +"%Y-%m-%d %H:%M:%S")"
-
-git add .
-git commit -m "$commit_message"
-git push
