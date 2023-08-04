d5bcfcf1b9d1bac85ea2c76104c0f8e9429c5801
WalaEddine01 <walaaeddine33@gmail.com>
2023-08-04 16:27:13 +0000

    I made The Changes at 2023-08-04 16:27:13

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,25 @@
 #include "main.h"
 /**
+ * print_binary - the binary representation of a number
+ * @n: input number
+ * Return: Nothing
  */
 void print_binary(unsigned long int n)
 {
+	unsigned int j = 0, l = 0;
 
+	if (n == 0)
+	{
+		printf ("0");
+		return;
+	}
+	l = n / 2;
+	j = n % 2;
+	while (l > 0)
+	{
+		printf("%u", j);
+		j = l % 2;
+		l = l / 2;
+	}
+	printf("%u", j);
+}
