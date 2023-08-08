f7b9e4413fb81b72880a3f712ce993bf659d9deb
WalaEddine01 <walaaeddine33@gmail.com>
2023-08-08 17:30:56 +0000

    I made The Changes at 2023-08-08 17:30:56

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,13 +8,14 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t fd, a = 0;
-	char buf[1024];
+	char *buf;
 
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+	buf = malloc(sizeof(char) * 1024);
 	a = read(fd, &buf[0], letters);
 	a = write(STDOUT_FILENO, buf, a);
 	close(fd);
