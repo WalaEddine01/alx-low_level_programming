2391f90b57bc3046e891db68fc62ea744f8b662f
WalaEddine01 <walaaeddine33@gmail.com>
2023-08-08 17:06:54 +0000

    I made The Changes at 2023-08-08 17:06:54

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,18 +7,18 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fd, n = 0, a = 0;
+	ssize_t fd, a = 0;
 	char buf[1024];
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1 || filename == NULL || letters == 0)
+	if (fd == -1 || filename == NULL)
 		return (0);
-	n = read(fd, &buf[0], letters);
-	if (n == -1)
+	a = read(fd, &buf[0], letters);
+	if (a == -1)
 		return (0);
-	a = write(STDOUT_FILENO, &buf[0], n);
+	a = write(STDOUT_FILENO, &buf[0], a);
 	if (a == -1)
 		return (0);
 	close(fd);
-	return (n);
+	return (a);
 }
