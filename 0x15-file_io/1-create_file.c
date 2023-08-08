e04b1058ee5e34145c7582c4c020b6990106c48d
WalaEddine01 <walaaeddine33@gmail.com>
2023-08-08 20:26:47 +0000

    I made The Changes at 2023-08-08 20:26:47

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 	{
 		close(a);
-		return (-1);
+		return (1);
 	}
 	a = write(a, text_content, strlen(text_content));
 	if (a == -1)
