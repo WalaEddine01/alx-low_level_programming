74108cb9730aa3befbba8b7fd54d3c84c846c5f4
WalaEddine01 <walaaeddine33@gmail.com>
2023-08-08 16:23:50 +0000

    I made The Changes at 2023-08-08 16:23:50

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,37 @@
 #include "main.h"
 /**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: the name of the file
+ * @letters: the number of letters should print
+ * Return: the actual number letters it could read and print
  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd, n = 0, i = 0, a = 0;
+	char buf[1024];
 
+	fd = open(filename, O_RDONLY);
+	if (fd == -1 || filename == NULL || letters == 0)
+	{
+		close(fd);
+		return (0);
+	}
+	n = read(fd, buf, letters);
+	if (n == -1 || n == 0)
+	{
+		close(fd);
+		return (0);
+	}
+	while (buf[i] != '\0')
+	{
+		a = write(STDOUT_FILENO, &buf[i], 1);
+		if (a == -1)
+		{
+			close(fd);
+			return (0);
+		}
+		i++;
+	}
+	close(fd);
+	return (n);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
