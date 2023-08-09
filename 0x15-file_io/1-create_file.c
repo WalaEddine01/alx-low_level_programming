1960bb0cd5c7b44e7f09e0b1143dd547c6f5ec8e
WalaEddine01 <walaaeddine33@gmail.com>
2023-08-09 20:00:55 +0000

    I made The Changes at 2023-08-09 20:00:55

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,6 +29,6 @@ int create_file(const char *filename, char *text_content)
 		close(a);
 		return (-1);
 	}
-	close(a);
+	close(fd);
 	return (1);
 }
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: the name of the file
+ * @text_content: the string to add at the end of the file
+ * Return: 1 on success and -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t fd;
+
+	fd = open(filename, O_RDONLY);
+	if (fd != -1)
+	{
+		fd = open(filename, O_WRONLY | O_APPEND);
+		if (fd == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+		if (!text_content)
+		{
+			close(fd);
+			return (1);
+		}
+		fd = write(fd, text_content, strlen(text_content));
+		if (fd == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+		close(fd);
+		return (1);
+	}
+	return (-1);
+}
