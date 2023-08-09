223572a2cffe58fb248a3ad8fad9862bf1404821
WalaEddine01 <walaaeddine33@gmail.com>
2023-08-09 21:28:52 +0000

    I made The Changes at 2023-08-09 21:28:52

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - copies the content of a file to another file
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	char *msg = "Usage: cp file_from file_to";
+
+	if (argc != 3)
+	{
+		write(1, msg, strlen)
+}
