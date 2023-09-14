847d81fcc602ad5b3fd99c54878e0b3f3032dfaf
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-14 22:10:53 +0000

    I made the changes on 2023-09-14 22:10:53

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -17,6 +17,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (*head != NULL)
 			(*head)->prev = NULL;
 		free(tmp);
+		tmp = NULL;
 		return (1);
 	}
 	while (index > 0)
