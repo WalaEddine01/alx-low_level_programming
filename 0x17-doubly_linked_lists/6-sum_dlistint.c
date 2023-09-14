acc27ff030767ad3c48d777a04048a6a4e75a845
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-14 19:33:07 +0000

    I made the changes on 2023-09-14 19:33:07

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_dlistint - returns the sum of all data of the list
+ * @head: pointer to the list
+ * Return: the sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int a = 0;
+
+	while (head)
+	{
+		a = a + head->n;
+		head = head->next;
+	}
+	return (a);
+}
