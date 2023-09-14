5e3a0bb37e281596097e39a8140dda417818317c
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-14 19:11:16 +0000

    I made the changes on 2023-09-14 19:11:16

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - return the node in the index
+ * @index: the index of the node
+ * @head: pointer to the list
+ * Return: the node in the index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+	if (index == 0)
+		return (head);
+	while (i < index && head && head->next)
+	{
+		head = head->next;
+		i++;
+	}
+	return (head);
+}
