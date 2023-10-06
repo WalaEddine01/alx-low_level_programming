161ff2c0484667cc4fef342dca76f0ef269f5909
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-06 16:40:33 +0000

    I made the changes on 2023-10-06 16:40:33

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *tmp2;
 
-	if (key == NULL || ht == NULL)
+	if (strlen(key) == 0 || ht == NULL || ht->array == NULL ||
+			ht->size == 0 || key == NULL)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
