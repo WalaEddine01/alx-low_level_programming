4d4f900b7de1156b96333159783676d0a9953d8b
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-06 16:50:43 +0000

    I made the changes on 2023-10-06 16:50:43

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,7 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (0);
 		tmp2->key = strdup(key);
 		if (tmp2->key == NULL)
-                        return (0);
+			return (0);
 		tmp2->value = strdup(value);
 		if (tmp2->value == NULL)
 			return (0);
