14faca91bff8f926488b4a477c0cf6b63c4fc8ed
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-06 18:52:59 +0000

    I made the changes on 2023-10-06 18:52:59

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,10 +1,13 @@
 #include "hash_tables.h"
 /**
+ * hash_table_get - get the element from the hash table
+ * @ht: the hash table
+ * @key: the key
+ * Return: the value of element
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = 0;
-	char *msg;
 
 	index = key_index((unsigned char *)key, ht->size);
 
@@ -15,19 +18,14 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 			while (ht->array[index])
 			{
 				if (strcmp(ht->array[index]->key, key) == 0)
-				{
-					msg = strdup(ht->array[index]->value);
-					return (msg);
-				}
+					return (ht->array[index]->value);
 				ht->array[index] = ht->array[index]->next;
 			}
 		}
 		else
 		{
 			if (strcmp(ht->array[index]->key, key) == 0)
-			{
-				return (msg);
-			}
+				return (ht->array[index]->value);
 		}
 	}
 	return (NULL);
