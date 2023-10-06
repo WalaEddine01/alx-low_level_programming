48af9a642f3dcd294da2aa37b94d4a8f05b0f17f
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-06 05:42:21 +0000

    I made the changes on 2023-10-06 05:42:21

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht;
 	int i = 0;
 
+	if (size <= 0)
+		return (NULL)
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
