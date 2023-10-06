b9cfaba94be10b5bcdebfaf8545b3d445ff308fe
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-06 21:37:26 +0000

    I made the changes on 2023-10-06 21:37:26

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+/**
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *tmp;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp)
+		{
+			free(tmp->key);
+			free(tmp->value);
+			tmp = tmp->next;
+		}
+		free(tmp);
+	}
+	free(ht);
+	free(tmp);
+}
