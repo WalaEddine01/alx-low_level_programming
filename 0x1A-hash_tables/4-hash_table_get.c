047c14aa525ca8702efc9b5f0a7efd5a82720002
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-07 09:43:32 +0000

    I made the changes on 2023-10-07 09:43:32

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,8 +10,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index = 0;
 	hash_node_t *tmp;
 
-	if (key == NULL || ht->array == NULL || strlen(key) == 0 ||
-			ht == NULL  || ht->size == 0)
+	if (key == NULL || strlen(key) == 0 || ht == NULL)
 		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index])
