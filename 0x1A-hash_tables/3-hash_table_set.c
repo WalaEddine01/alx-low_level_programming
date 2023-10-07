656ac7454e88c9973bac6fa34dc5eb6e2391b6f3
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-07 08:12:25 +0000

    I made the changes on 2023-10-07 08:12:25

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -31,7 +31,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(tmp2);
 		return (0);
 	}
-	tmp2->next = ht->array[index];
+	if (ht->array[index])
+		tmp2->next = ht->array[index];
+	else
+		tmp2->next = NULL;
 	ht->array[index] = tmp2;
 	return (1);
 }
